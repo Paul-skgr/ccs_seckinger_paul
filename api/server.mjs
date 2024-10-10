6b59bde713374eda335fce717914339c2ccd0afb
Paul seckinger <paulseckinger@MacBook-Air-de-Paul.local>
2024-10-10 12:08:20 +0200

    Voume:connexion API:OK

--- api/server.mjs
+++ api/server.mjs
@@ -1,13 +1,14 @@
 import Fastify from 'fastify' 
-
+import { featureStore } from './featureStore.mjs'
 
 export function createServer(){
     const fastify = Fastify({
         logger:true
     })    
-    fastify.get("/feature/:code", function(req, res){
+    fastify.get("/feature/:code", async function(req, res){
         const code =req.params.code
-        res.send({"code":code,"enabled": true});
+        const enable= await featureStore.isEnabled(code);
+        res.send({"code":code, "enabled": enable});  
     })
     return fastify;
 }
