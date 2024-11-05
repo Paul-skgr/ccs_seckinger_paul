77680e75fa5b3222fd97a3fe8f0c9515934573e5
Paul seckinger <paulseckinger@MacBook-Air-de-Paul.local>
2024-11-05 17:23:27 +0100

    passworddigit + length+ letter

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,13 +1,9 @@
 function isValidpassword(password){
-    const length = password.length >= 8;
+    const isLengthValid = password.length >= 8;
     const hasdigit = /\d/.test(password);
     const hasletter = /[a-zA-Z]/.test(password); 
 
-    return length && hasdigit && hasletter;
+    return isLengthValid && hasdigit && hasletter;
 }
 
 module.exports = isValidpassword;
-
-
-
-
