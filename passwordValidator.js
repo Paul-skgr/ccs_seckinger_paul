cbafad6d3196a3448c7d0cc3ee30ae859dec696f
Paul seckinger <paulseckinger@MacBook-Air-de-Paul.local>
2024-11-05 17:44:59 +0100

    password length TDD

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,9 +1,6 @@
 function isValidpassword(password){
     const isLengthValid = password.length >= 8;
-    const hasdigit = /\d/.test(password);
-    const hasletter = /[a-zA-Z]/.test(password); 
-
-    return isLengthValid && hasdigit && hasletter;
+    return isLengthValid
 }
 
 module.exports = isValidpassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -6,21 +6,8 @@ test ('Password length 8', ()=>{
     expect(isValid).toBe(true);
 });
 
-test ('Password has at least 1 digit', ()=>{
-    const passwrd1="azerty1uiop";
-    const passwrd="azertyuiop";
-    const isValid =isValidpassword(passwrd1);
-    expect(isValid).toBe(true);
-    const isValid1 =isValidpassword(passwrd);
-    expect(isValid1).toBe(false);
-});
-
-
-test ('Password has at least 1 letter', ()=>{
-    const passwrd1="azerty1uiop";
-    const passwrd="1582687";
-    const isValid =isValidpassword(passwrd1);
-    expect(isValid).toBe(true);
-    const isValid1 =isValidpassword(passwrd);
-    expect(isValid1).toBe(false);
+test ('Password length 8', ()=>{
+    const passwrd="ffh";
+    const isValid =isValidpassword(passwrd);
+    expect(isValid).toBe(false);
 });
