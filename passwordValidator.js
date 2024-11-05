f8c3e14de6bafe2187981c5978c5259e5209aa68
Paul seckinger <paulseckinger@MacBook-Air-de-Paul.local>
2024-11-05 16:56:53 +0100

    password

---
+++ passwordValidator.js
@@ -0,0 +1,5 @@
+function isValidpassword(password){
+    return password.length >= 8;
+}
+
+module.exports = isValidpassword;
---
+++ passwordValidator.spec.js
@@ -0,0 +1,16 @@
+const isValidpassword= require('./passwordValidator.js');
+
+test ('Password length 8', ()=>{
+    const passwrd="azertyuiop1";
+    const isValid =isValidpassword(passwrd);
+    expect(isValid).toBe(true);
+});
+
+test ('Password has at least 1 digit', ()=>{
+    const passwrd1="azerty1uiop";
+    const passwrd="azertyuiop";
+    const isValid =isValidpassword(passwrd1);
+    expect(isValid).toBe(true);
+    const isValid1 =isValidpassword(passwrd);
+    expect(isValid1).toBe(false);
+});
