5b68ecb8e21265c21aa0dcc4576e90ad70623285
Paul seckinger <paulseckinger@MacBook-Air-de-Paul.local>
2024-11-19 16:08:56 +0100

    BDD dédiée aux query

--- CQRS/accountCommand.mjs
+++ CQRS/accountCommand.mjs
@@ -1,19 +1,28 @@
 import { Account } from "./account.mjs";
 import { commandDAO } from "./accountCommandDAO.mjs";
 import { accountQuery } from "./accountQuery.mjs";
+import { ACCOUNT_LIST } from "./database.mjs";
+import { queryDatabase } from "./queryDatabase.mjs";
+
 
 function addAccount(lastName, firstName) {
     const account = new Account(null, lastName, firstName, null);
-    commandDAO.insertAccount(account);
+    commandDAO.insertAccount(account, ACCOUNT_LIST);
+    const queryAccount = { lastName, firstName };
+    commandDAO.insertAccount(queryAccount, queryDatabase)
 }
+
+
 
 function saveAccount(id,  lastName, firstName){
     const newAccountData = accountQuery.getAccount(id);
     newAccountData.lastName = lastName;
     newAccountData.firstName = firstName;
+    const queryAccount = { lastName, firstName };
     console.log("yo", newAccountData);
-    return commandDAO.updateAccount(id , newAccountData);
+    return commandDAO.updateAccount(id , newAccountData, ACCOUNT_LIST), commandDAO.updateAccount(id, queryAccount, queryDatabase);
 }
+
 
 
 export const accountCommand = {
--- CQRS/accountQueryDAO.mjs
+++ CQRS/accountQueryDAO.mjs
@@ -1,11 +1,13 @@
 import { ACCOUNT_LIST } from "./database.mjs";
 
+import { queryDatabase } from "./queryDatabase.mjs";
+
 function retrieveAccountList(deletedItem){
     if (deletedItem == "creationDate"){
         return ACCOUNT_LIST.map(({ creationDate, ...rest }) => rest);
     }
     else{
-        return ACCOUNT_LIST;
+        return queryDatabase;
         
     }
 }
